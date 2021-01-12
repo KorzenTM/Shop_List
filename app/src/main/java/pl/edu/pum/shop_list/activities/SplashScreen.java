187533697812b77eb8c3174dcf54b7e040d1743b
Mateusz Korzeniowski <mateuszkorzeniowski98@gmail.com>
2021-01-12 17:52:01 +0100

    Create SplashScreen

---
+++ app/src/main/java/pl/edu/pum/shop_list/activities/SplashScreen.java
@@ -0,0 +1,55 @@
+package pl.edu.pum.shop_list.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import pl.edu.pum.shop_list.R;
+
+public class SplashScreen extends AppCompatActivity
+{
+    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progress);
+
+        ActivityStarter starter = new ActivityStarter();
+        starter.start();
+    }
+
+    private class ActivityStarter extends Thread
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                {
+                    //TODO read values from database
+                    progressBar.setVisibility(View.VISIBLE);
+                    Thread.sleep(SPLASH_SCREEN_TIMEOUT);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.d("SplashScreen", e.getMessage());
+            }
+
+            progressBar.setVisibility(View.INVISIBLE);
+            Intent intent = new Intent(SplashScreen.this, ShopListsActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
---
+++ app/src/main/java/pl/edu/pum/shop_list/fragments/ShopListsFragment.java
@@ -0,0 +1,35 @@
+package pl.edu.pum.shop_list.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.edu.pum.shop_list.R;
+
+public class ShopListsFragment extends Fragment
+{
+
+
+    public ShopListsFragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View v =  inflater.inflate(R.layout.fragment_shop_lists, container, false);
+        return v;
+    }
+}
